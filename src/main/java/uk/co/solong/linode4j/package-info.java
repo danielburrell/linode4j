3e60e19fa702d11504175ed1b0f0227550931ea8
danielburrell <daniel.burrell@gmail.com>
2015-03-14 05:35:39 +0000

    fix build warnings
    fix build warnings

--- src/main/java/uk/co/solong/linode4j/package-info.java
+++ src/main/java/uk/co/solong/linode4j/package-info.java
@@ -1,6 +1,8 @@
 /**
  * All source code is generated on the fly by the rest2java plugin.
  * 
- * Files are written to target/java-gen
+ * Files are written to /target/java-gen
+ * 
+ * Additional hand written utilities can be placed in the /src/main/java area
  */
 package uk.co.solong.linode4j;
