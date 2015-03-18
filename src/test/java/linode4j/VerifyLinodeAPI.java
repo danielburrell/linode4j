e4553485973fce0e78ab02331983c41174ca2fb6
danielburrell <daniel.burrell@gmail.com>
2015-03-18 00:42:00 +0000

    finished linode schema
    finished linode schema without return types

---
+++ src/test/java/linode4j/VerifyLinodeAPI.java
@@ -0,0 +1,24 @@
+package linode4j;
+
+import org.junit.Test;
+
+import uk.co.solong.linode4j.Linode;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public class VerifyLinodeAPI {
+
+    @Test
+    public void test() throws JsonProcessingException {
+        Linode t = new Linode("notrequired");
+        String apiSpec = t.apiSpec().go();
+        System.out.println(apiSpec);
+        //verify the following
+        //every method in the api exists
+        //for every method, every optional parameter exists
+        //for every method, every mandatory parameter exists
+        //for every method, every parameter type is correct
+        //for every method, every parameter json value is correct
+    }
+
+}
