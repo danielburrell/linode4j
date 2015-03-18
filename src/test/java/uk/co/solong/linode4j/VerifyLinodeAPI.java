8799ad7196c420a08ed4eef8d931a48a72f5b8d5
danielburrell <daniel.burrell@gmail.com>
2015-03-18 01:31:12 +0000

    adjusted pom to new plugin name
    adjusted pom to new plugin name

--- src/test/java/uk/co/solong/linode4j/VerifyLinodeAPI.java
+++ src/test/java/uk/co/solong/linode4j/VerifyLinodeAPI.java
@@ -5,13 +5,15 @@ import org.junit.Test;
 import uk.co.solong.linode4j.Linode;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class VerifyLinodeAPI {
 
     @Test
     public void apiLint() throws JsonProcessingException {
         Linode t = new Linode("notrequired");
-        String apiSpec = t.apiSpec().go();
+        String apiSpec = t.apiSpec().asString();
         System.out.println(apiSpec);
         //verify the following
         //every method in the api exists
